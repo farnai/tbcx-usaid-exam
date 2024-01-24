7718ab2af74edf4c36778ea86ee7d985d34fdc04
farnai <f.iordanishvili@gmail.com>
2024-01-25 03:21:15 +0400

    Create animation and structure

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,4 @@
+/// header stick function
 document.addEventListener("DOMContentLoaded", function () {
     var header = document.getElementById("header-sticky");
     var scrollPos = 0;
@@ -16,3 +17,47 @@ document.addEventListener("DOMContentLoaded", function () {
         scrollPos = currentScrollPos;
     });
 });
+
+//slider
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Function to advance to the next slide
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Function to set the current slide
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+// Function to show slides
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+// Autoplay function
+function autoPlay() {
+    plusSlides(1);
+}
+
+// Set interval for autoplay (change the value in milliseconds as needed)
+setInterval(autoPlay, 3000);
+// Adjust the interval as needed (e.g., 2000 for 2 seconds)
